c368aedcc0926a3946765202d3ab0b10e565d918
Rishikul Borah <rishikulborah@gmail.com>
2026-04-21 20:15:25 +0530

    2nd Updatation after lead's suggestion

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,7 @@ const creativeEvents = [
       },
       {
         id: 't7',
-        title: 'Tech Quiz',
+        title: 'Brainy Bowl',
         image: '/quiz_event.png',
         link: 'https://docs.google.com/forms/d/e/1FAIpQLSe1b3cFVmnGWyQjou_APB8GB0Q3UAOB_uEQ5R3h_OTvdhLpJg/viewform?usp=sharing&ouid=102858058196643049393',
         prizes: '₹2,000',
@@ -208,8 +208,8 @@ const creativeEvents = [
         image: '/digital_art.png',
         link: 'https://docs.google.com/forms/d/e/1FAIpQLSdRtivMkleBqFeIxGa9SrEwjlt01iJQrziijnP46buaQKVt3g/viewform?usp=sharing&ouid=102858058196643049393',
         prizes: 'Winner:₹2,000 | Runner-up:₹1,000',
-        rules: 'Theme: Cinematic Tech. Originals only.',
-        process: 'Register -> Concept -> Submission -> Screening'
+        rules: 'Open to all college/university students. Submission must be original and theme-based. File format: PNG (Max size: 10MB). Rename file as: YourName_CollegeName. Any digital tools allowed (No AI usage). Judging: creativity, originality, relevance. No offensive/inappropriate content. Judges’ decision is final',
+        process: 'Online → Topic Reveal (2 Days Prior) → Pre-Submission (1st May | Before 12 PM) → Screening'
       }
     ]
   }
@@ -298,7 +298,7 @@ const committeeMembers = [
     members: [
       { name: "Birochan Das", linkedin: "#", insta: "https://instagram.com/__biru__09", photo: "/committee_picture/BIRU - Birochan Das.jpeg" },
       { name: "Rudra Upadhyaya", linkedin: "https://www.linkedin.com/in/rudra-upadhyaya-235788217/", insta: "https://instagram.com/upadhyaya_rudra56", photo: "/committee_picture/IMG_4544 - Rudra Upadhyaya.jpeg" },
-      { name: "Aritram Dutta", linkedin: "#", insta: "#" }
+      { name: "Aritram Dutta", linkedin: "https://www.linkedin.com/in/aritram-dutta-1189b4205?utm_source=share_via&utm_content=profile&utm_medium=member_android", insta: "https://www.instagram.com/aritram__?igsh=MXQ2cXFkaGpsem9zZA==" }
     ]
   }
 ];
@@ -1097,19 +1097,28 @@ function App() {
               <div style={{ textAlign: 'center' }}>
                 <h3>PARTNERS & COLLABORATORS</h3>
               </div>
+
               <div className="sponsor-boxes">
                 <div className="sponsor-box small">
                   <img src="/.xyz.png" alt=".XYZ Domain" className="sponsor-logo" />
                   <span className="sponsor-name">.xyz (Domain Partner)</span>
                 </div>
-                <div className="sponsor-box small">
-                  <img src="/web3assam_logo.jpg" alt="Web3Assam" className="sponsor-logo" />
-                  <span className="sponsor-name">Web3Assam (Community Partner)</span>
-                </div>
+                <a href="https://www.instagram.com/creativlok_llp/" target="_blank" rel="noopener noreferrer">
+                  <div className="sponsor-box small">
+                    <img src="/CreativLok.png" alt="CreativLok" className="sponsor-logo" />
+                    <span className="sponsor-name">CreativLok (Hiring Partner)</span>
+                  </div>
+                </a>
                 <div className="sponsor-box small">
                   <img src="/frint.webp" alt="Frint.in" className="sponsor-logo" />
                   <span className="sponsor-name">Frint.in (Hiring Partner)</span>
                 </div>
+
+                <div className="sponsor-box small">
+                  <img src="/web3assam_logo.jpg" alt="Web3Assam" className="sponsor-logo" />
+                  <span className="sponsor-name">Web3Assam (Community Partner)</span>
+                </div>
+
                 <div className="sponsor-box small">
                   <img src="/api_community.jpg" alt="The API Community" className="sponsor-logo" />
                   <span className="sponsor-name">The API Community (Community Partner)</span>
